ce7021f6c50fc17265e23797a9d1b58e87eb648d
shirenekboyd <shirenekadkhodai@gmail.com>
2022-12-05 13:59:33 -0600

    updated ExpenseItem with dummy data and invoked it twice in the App.js file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div>
       <ExpenseItem />
+      <ExpenseItem />
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  // dummy data to mimic dynamic data
+const expenseData = new Date(2021, 2, 28);//creating a date object
+const expenseTitle = "Car Insurance";
+const expenseAmount = 294.67;
+
   return (
     // <h2>Expense item!</h2>
     <div className="expense-item">
-      <div className="expense-item_description">March 28th 2021</div>
-      <h2>Car Insurance</h2>
-      <div className="expense-item__price">$294.67</div>
+      <div className="expense-item_description">{expenseData.toISOString()}</div>
+      <h2>{expenseTitle}</h2>
+      <div className="expense-item__price">${expenseAmount}</div>
     </div>
   );
 }
