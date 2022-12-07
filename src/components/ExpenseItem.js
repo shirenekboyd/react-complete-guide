9e9311a0a5e4a237bcafe8e32eee1bd1a1f2b32b
shirenekboyd <shirenekadkhodai@gmail.com>
2022-12-07 16:10:16 -0600

    updated ExpenseItem.js with dynamic data by passing props from App.js and rendering it in the JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,31 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  // dummy data to mimic dynamic data
-const expenseData = new Date(2021, 2, 28);//creating a date object
-const expenseTitle = "Car Insurance";
-const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  // parameter for passing "props"
+
+  const month = props.date?.toLocaleString("en-US", { month: "long" });
+  const day = props.date?.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date?.getFullYear();
+
+  //------ dummy data to mimic dynamic data ------//
+  // const expenseData = new Date(2021, 2, 28);//creating a date object
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+
+  console.log("Date", props.date);
 
   return (
-    // <h2>Expense item!</h2>
     <div className="expense-item">
-      <div className="expense-item_description">{expenseData.toISOString()}</div>
-      <h2>{expenseTitle}</h2>
-      <div className="expense-item__price">${expenseAmount}</div>
+      {/* <div>{props.date?.toLocaleString()}</div> */}
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
