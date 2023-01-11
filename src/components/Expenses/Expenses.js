ef1c261a91d0cc78bb02d502c23c56ad5dd92c02
shirenekboyd <shirenekadkhodai@gmail.com>
2023-01-11 14:32:21 -0600

    created an ExpensesList component that checks the expense items conditionally

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
-import ExpenseItem from "./ExpenseItem";
+
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "../ExpensesList/ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -18,19 +19,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear; //if truthy render only items with that selected year else don't
   });
 
-  //stored jsx content inside a variable
-  let expensesContent = <p>No expenses found.</p>;
-  //checking for an empty list
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id} //each item has a unique id, see dummy expenses in App.js
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  
 
   return (
     <div>
@@ -39,7 +28,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
