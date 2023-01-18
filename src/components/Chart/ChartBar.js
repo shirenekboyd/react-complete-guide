66dfe217d073bc4ef89571321f6a79f700c02f26
shirenekboyd <shirenekadkhodai@gmail.com>
2023-01-18 08:44:21 -0600

    added some more comments to ChartBar component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,13 +4,14 @@ const ChartBar = (props) => {
   let barFillHeight = "0%";
 
   if (props.max > 0) {
+    //rounding to nearest integer
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        {/* style wants a javaScript object */}
+        {/* style below wants a javaScript object, property names are keys, value is value*/}
         <div
           className="chart-bar__fill"
           style={{ height: barFillHeight }}
