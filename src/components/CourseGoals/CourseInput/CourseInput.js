be556d02718ce998e99d67e978c72ef4b4d8a911
shirenekboyd <shirenekadkhodai@gmail.com>
2023-01-24 13:39:49 -0600

    added styled component and media query to Button.js

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,36 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  /* setting css classes dynamically with css rules */
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,10 +59,16 @@ const CourseInput = (props) => {
     <form onSubmit={formSubmitHandler}>
       {/* setting css classes dynamically with template literals instead of inline styling */}
       {/* className is either set to a string 'form-control' or 'form-control invalid' */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+
+      {/* using props and template literals in the styled.div component up top */}
+      <FormControl invalid={!isValid}>
+      {/* if invalid is true then that class is applied, if false then nothing is applied */}
+        {/* <FormControl className={!isValid && 'invalid'}> */}
+        {/* <div className={`form-control ${!isValid ? "invalid" : ""}`}> */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+        {/* </div> */}
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
