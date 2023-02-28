fae70ceb0134227f6fb4d1ffe27f68abcab9864f
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-02-28 14:01:27 -0600

    updated App.js and index.css, added a components directory, UI and Users directory along with new files for Button, Card, AddUser

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+const addUserHandler = (event) => {
+event.preventDefault();
+}
+
+  return (
+    <Card className={classes.input}>
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number"></input>
+      <Button type="submit">Add User</Button>
+    </form>
+    </Card>
+  );
+};
+
+export default AddUser;
