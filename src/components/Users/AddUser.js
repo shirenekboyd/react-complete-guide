9c45d8563a682c132a2116d887dec9393cc20127
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-03-14 16:26:54 -0500

    added useRef hooks to AddUser component and commented out unneccessary code since created useRef variables

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -6,14 +6,22 @@ import classes from "./AddUser.module.css";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUsername, setEnteredUsername] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    // console.log(nameInputRef.current.value);
+      //variables below that store the useRef values
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
     //validation
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
@@ -21,27 +29,33 @@ const AddUser = (props) => {
       return;
     }
     // '+' prefix ensures you are comparing a number with a number not a string
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    console.log("Inputs: ", enteredUsername, enteredAge);
-    props.onAddUser(enteredUsername, enteredAge);
+    console.log("Inputs: ", enteredName, enteredUserAge);
+    props.onAddUser(enteredName, enteredUserAge);
+    // resets the user input field using useRef() hook and manually manipulating the DOM; do this very RARELY!
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+
+    //----Using useRef() hook to get the enteredUsername and enteredAge inputs so don't need below code----
     //reseting the input field along with 'value' props with inputs in JSX reflecting the stored state
-    setEnteredUsername("");
-    setEnteredAge("");
+    // setEnteredUsername("");
+    // setEnteredAge("");
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  //----2 state updating handlers below no longer needed because of useRef()-----------//
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   //handles the error
   const errorHandler = () => {
@@ -63,15 +77,19 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // no longer needed 
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            ref={nameInputRef}
           ></input>
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // no longer needed
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           ></input>
           <Button type="submit">Add User</Button>
         </form>
